baaece69c27e2f1473bac08f80b1d037900879fc
Ben Osborn <benosborntech@gmail.com>
2025-12-14 11:06:15 +1100

    chore: works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod client;
 
+use std::cmp;
+use std::io::{self, ErrorKind};
+use std::num::ParseIntError;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -13,31 +16,53 @@ async fn main() -> tokio::io::Result<()> {
     let listener = TcpListener::bind(addr).await?;
 
     loop {
-        let (mut socket, addr) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
         tokio::spawn(handle_client(socket));
     }
 }
 
 async fn handle_client(mut socket: TcpStream) -> tokio::io::Result<()> {
     let mut out: Option<String> = None;
-    let mut buf = [0; MAX_BUF_SIZE];
 
-    if let Ok(n) = socket.read(&mut buf).await {
+    loop {
+        let mut buf = [0; MAX_BUF_SIZE];
+        let n = socket.read(&mut buf).await?;
         let message = String::from_utf8_lossy(&buf[..n]);
+
+        let mut start: usize = 0;
+
         if out.is_none() {
-            let mut msg_size = String::with_capacity(MAX_HEADER_SIZE);
-            for c in message.chars() {
-                if c == DELIM {
-                    let size = match msg_size.parse::<u32>() {
-                        Ok(val) => Some(val),
-                        _ => None,
-                    };
-                    break;
-                }
-                msg_size.push(c);
-            }
+            let (start_idx, size) = parse_header(&message)
+                .map_err(|_| io::Error::new(ErrorKind::InvalidData, "failed to parse header"))?;
+            start = start_idx;
+            out = Some(String::with_capacity(size));
         }
-        let _ = socket.write_all(&buf[..n]).await;
+
+        let str_buf = out.as_mut().unwrap();
+        let remaining = str_buf.capacity() - str_buf.len();
+        let end: usize = cmp::min(MAX_BUF_SIZE, remaining);
+
+        for c in message.chars().skip(start).take(end - start) {
+            str_buf.push(c);
+        }
+
+        if str_buf.capacity() == str_buf.len() {
+            println!("This is the message: {}", str_buf);
+            break;
+        }
     }
+
     Ok(())
+}
+
+fn parse_header(msg: &str) -> Result<(usize, usize), ()> {
+    let mut msg_size = String::with_capacity(MAX_HEADER_SIZE);
+    for (idx, c) in msg.chars().enumerate() {
+        if c == DELIM {
+            let size = msg_size.parse::<usize>().map_err(|_| ())?;
+            return Ok((idx + 1, size));
+        }
+        msg_size.push(c);
+    }
+    Err(())
 }
